44e0f03dba8f9c54a4489632d0dca7682784b5d6
SushantSail <133793150+SushantSail@users.noreply.github.com>
2026-03-13 00:16:49 +0530

    debouncing + Map method

--- src/components/Debouncing.jsx
+++ src/components/Debouncing.jsx
@@ -1,55 +1,59 @@
-import React, { useState, useEffect, useCallback } from "react";
-import "./style.css";
+import React, { useState, useEffect, useCallback } from 'react';
 
-export default function Debouncing() {
-  const [word, setWord] = useState("");
+export default function App() {
+  const [word, setWord] = useState('');
+  const [data, setData] = useState([]);
+  const[loading ,setLoading] = useState(false);
 
-  async function getData(search) {
+  async function getApi(input) {
     try {
-      const res = await fetch(
-        `https://jsonplaceholder.typicode.com/users`
-      );
-      const data = await res.json();
-      console.log(data);
+      const res = await fetch('https://jsonplaceholder.typicode.com/users');
+      const result = await res.json();
+      console.log(input,':',result);
+      setData(result);
+      setLoading(false);
     } catch (err) {
-      console.log("Error Occured", err);
+      console.log('Error Occured', err);
     }
   }
 
   function debounce(fn, delay) {
     let timer;
-
     return function (value) {
       clearTimeout(timer);
-
       timer = setTimeout(() => {
         fn(value);
       }, delay);
     };
   }
 
-  const debounceRes = useCallback(debounce(getData, 5000), []);
+  const debounceFn = useCallback(debounce(getApi, 3000), []);
 
   useEffect(() => {
     if (word) {
-      debounceRes(word);
+      debounceFn(word);
     }
-  }, [word, debounceRes]);
+  }, [word, debounceFn]);
 
   return (
     <div>
-      <h1>Debouncing</h1>
+      <input type="text" onChange={(e) => {setWord(e.target.value); setLoading(true) }} />
+      <button>Search</button>
 
-      <input
-        type="text"
-        value={word}
-        onChange={(e) => setWord(e.target.value)}
-      />
+      <hr />
+      <ul>
+{
+  loading ? 'Loading the Data' : 
+(
+ data.map((item)=>(
+    <li key={item.id}>{item.name}</li>
+  ))
+)
+}
 
-      <br />
-      <br />
 
-      <p>Debounced word: {word}</p>
+     
+</ul>
     </div>
   );
 }
